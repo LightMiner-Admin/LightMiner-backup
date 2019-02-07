581a0c9aecfb794ff0032ce0a19751fbb00b3ed0
ALPHA2004 <37263152+ALPHA2004@users.noreply.github.com>
2019-02-07 11:07:02 +0800

    Update

--- zh/pwabuilder-sw-register.js
+++ zh/pwabuilder-sw-register.js
@@ -7,7 +7,7 @@ if (navigator.serviceWorker.controller) {
 
 //Register the ServiceWorker
   navigator.serviceWorker.register('pwabuilder-sw.js', {
-    scope: './zh/'
+    scope: './'
   }).then(function(reg) {
     console.log('Service worker has been registered for scope:'+ reg.scope);
   });
